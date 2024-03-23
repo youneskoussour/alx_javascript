868ae233e0b30ed892d326109e51963c32014c47
youneskoussour <younesksr99@gmail.com>
2024-03-23 14:28:08 +0000

    commit

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+const first_line = "C is fun";
+const second_line ="Python is cool";
+const third_line = "JavaScript is amazing";
+
+console.log(first_line);
+console.log(second_line); 
+console.log(third_line);
---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,5 @@
+const Languages =["C is fun", "Python is cool", "JavaScript is amazing"]
+
+for (let i = 0; i < Languages.length; i++){
+    console.log(Languages[i]);
+}
